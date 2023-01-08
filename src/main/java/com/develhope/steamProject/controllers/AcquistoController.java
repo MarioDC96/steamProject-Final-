1fa6e0a85a2ebaffc9098249c6241f558f1d6fd6
Antonio Fasanella <fasanella.antonio.sanfele@gmail.com>
2023-01-08 19:06:05 +0100

    implement acquisto

--- src/main/java/com/develhope/steamProject/controllers/AcquistoController.java
+++ src/main/java/com/develhope/steamProject/controllers/AcquistoController.java
@@ -1,10 +1,22 @@
 package com.develhope.steamProject.controllers;
 
+import com.develhope.steamProject.services.AcquistoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("acquisto")
 public class AcquistoController {
 
+    @Autowired
+    AcquistoService acquistoService;
+
+    @PostMapping("")
+    public HttpStatus acquisto(@RequestParam long idUtente, @RequestParam long idVideogioco) {
+        return acquistoService.acquisto(idUtente, idVideogioco);
+    }
 }
